2b727f424bd43be26b7ccdfdca060f8f47e7c215
HryshcIlya <175040986+HryshcIlya@users.noreply.github.com>
2026-01-24 17:14:51 +0500

    chore(repo): remove upstream files and caches
    - Remove GitHub FUNDING.yml and Gitee mirror workflow
    
    - Remove unused cachix cache references

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -29,13 +29,10 @@
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
       "https://nix-community.cachix.org"
-      # my own cache server, currently not used.
-      # "https://ryan4yin.cachix.org"
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "ryan4yin.cachix.org-1:Gbk27ZU5AYpGS9i3ssoLlwdvMIh0NxG0w8it/cv9kbU="
     ];
     builders-use-substitutes = true;
   };
