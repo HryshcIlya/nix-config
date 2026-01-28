6b31b475070e60408f2952a05eaea365f7956f84
HryshcIlya <175040986+HryshcIlya@users.noreply.github.com>
2026-01-28 19:54:06 +0500

    chore(fcitx5): remove CJK input methods and overlays

--- home/linux/gui/base/fcitx5/default.nix
+++ home/linux/gui/base/fcitx5/default.nix
@@ -8,8 +8,6 @@
       # so we need to force replace it in every rebuild to avoid file conflict.
       force = true;
     };
-    "mozc/config1.db".source =
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/linux/gui/base/fcitx5/mozc-config1.db";
   };
 
   i18n.inputMethod = {
@@ -19,18 +17,6 @@
     fcitx5.addons = with pkgs; [
       qt6Packages.fcitx5-configtool # GUI for fcitx5
       fcitx5-gtk # gtk im module
-
-      # Chinese
-      fcitx5-rime # for flypy chinese input method
-      # fcitx5-chinese-addons # we use rime instead
-
-      # Japanese
-      # ctrl-i / F7 - convert to takakana
-      # ctrl-u / F6 - convert to hiragana
-      fcitx5-mozc-ut # Moze with UT dictionary
-
-      # Korean
-      fcitx5-hangul
     ];
   };
 }
