8beb6cf9d5818ccab8e054fa37bed281adf5fa6a
HryshcIlya <175040986+HryshcIlya@users.noreply.github.com>
2026-02-06 19:55:50 +0500

    fix(treesitter): stop ipkg parser extraction failures
    Remove orgmode's ensure_installed='all' override and ignore ipkg parser installs to prevent startup-time tarball extraction errors from parser downloads.

--- home/base/tui/editors/neovim/nvim/lua/plugins/orgmode.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/orgmode.lua
@@ -8,12 +8,5 @@ return {
       org_agenda_files = "~/org/**/*",
       org_default_notes_file = "~/org/refile.org",
     })
-
-    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-    -- add ~org~ to ignore_install
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = 'all',
-      ignore_install = { 'org' },
-    })
   end,
 }
